0e8c3e65770c9a60cca9d05b99dd9c1b1b884f48
chrn <854243450@qq.com>
2020-11-07 16:31:42 +0800

    chen

--- yoga/yoga-venue/src/main/java/com/woniu/testcontroller/Controller.java
+++ yoga/yoga-venue/src/main/java/com/woniu/testcontroller/Controller.java
@@ -1,6 +1,7 @@
 package com.woniu.testcontroller;
 
 
+import com.woniu.yoga.domain.TCoach;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +11,6 @@ public class Controller {
     @RequestMapping("/test")
     public void Test1(){
         System.out.println("123123213");
+        TCoach t = new TCoach();
     }
 }

78c8ea5546be7a7864f806409f052106749e24bc
1111 <1111111>
2020-11-09 18:37:09 +0800

    提交

--- yoga/yoga-common/utils/src/main/java/com.woniu.yoga/domain/TStudent.java
+++ yoga/yoga-common/utils/src/main/java/com.woniu.yoga/domain/TStudent.java
@@ -2,6 +2,7 @@ package com.woniu.yoga.domain;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
--- yoga/yoga-student/src/main/java/com/woniu/controller/TStudentController.java
+++ yoga/yoga-student/src/main/java/com/woniu/controller/TStudentController.java
@@ -5,14 +5,17 @@ import com.woniu.entity.TStudentDto;
 import com.woniu.entity.TStudentParam;
 import com.woniu.myexception.MyException;
 import com.woniu.service.TStudentService;
+import com.woniu.utils.EmailUtil;
 import com.woniu.utils.JsonResult;
 import com.woniu.utils.JwtUtil;
 import com.woniu.yoga.domain.TStudent;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
 
@@ -47,5 +50,33 @@ public class TStudentController {
         }
         return new JsonResult("200","success",null,studentDto);
     }
+    @GetMapping("qqLogin")
+    public JsonResult qqLogin(String code) throws Exception{
+        String url = "https://graph.qq.com/oauth2.0/token?grant_type=authorization_code&client_id=101780702&client_secret=be3cf79b89eafe3f0b8f90462aed8065&code="+code+"&redirect_uri=http://www.pawntest.com/qqlogin";
+        RestTemplate restTemplate = new RestTemplate();
+        String value = restTemplate.getForObject(url, String.class);
+        String token = value.substring(value.indexOf("=")+1,value.indexOf("&"));
+        url = "https://graph.qq.com/oauth2.0/me?access_token="+token;
+        value = restTemplate.getForObject(url, String.class);
+        String openId = value.substring(value.lastIndexOf(":")+2,value.lastIndexOf("\""));
+        url = "https://graph.qq.com/user/get_user_info?access_token="+token+"&oauth_consumer_key=101780702&openid="+openId;
+        String forObject = restTemplate.getForObject(url, String.class);
+        TStudentDto studentDto = tStudentService.qqLogin(openId);
+        return new JsonResult("200","success",null,studentDto);
+    }
+    @PostMapping("qqRegister")
+    public JsonResult qqRegister() throws Exception{
+        return new JsonResult();
+    }
+    @PostMapping("register")
+    public JsonResult register() throws Exception{
+        return new JsonResult();
+    }
+    @GetMapping("/code")
+    public JsonResult code(String email) throws Exception{
+        EmailUtil.sendCode(email);
+        return new JsonResult();
+    }
+
 }
 
